e9b0e205556be2b66f95302ebb908fa2c8a99ebf
Vinicius Moreira <vinicius.moreira.995@gmail.com>
2025-11-18 20:43:10 -0300

    Atividades dia 18/11

--- Atividade Pratica 05/Exercicio 1.py
+++ Atividade Pratica 05/Exercicio 1.py
@@ -1,18 +1,9 @@
-'''Enunciado: Crie uma função que calcule a gorjeta a ser deixada em um
-restaurante, baseada no valor total da conta e na porcentagem de gorjeta
-desejada.
-
-Calcula o valor da gorjeta baseado no total da conta e na porcentagem desejada.
-
+'''1 - Crie uma função que calcule a gorjeta a ser deixada em um restaurante, baseada no valor total da conta e na porcentagem de
+gorjeta desejada. Calcula o valor da gorjeta baseado no total da conta e na porcentagem desejada.
 Parâmetros:
-
-valor_conta (float): O valor total da conta
-
-porcentagem_gorjeta (float): A porcentagem da gorjeta (ex: 15 para 15%)
-
-Retorna:
-
-float: O valor da gorjeta calculada'''
+a - valor_conta (float): O valor total da conta
+b - porcentagem_gorjeta (float): A porcentagem da gorjeta (ex: 10 para 10%)
+c - retorna: float: O valor da gorjeta calculada'''
 
 
 def valor_gorjeta(valor_conta, porcentagem_gorjeta):
@@ -32,5 +23,4 @@ try:
 
 except ValueError:
     print("Digite apenas números.")
-    exit()
 
